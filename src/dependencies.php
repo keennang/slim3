3aa9fe3efb80e886f12c4db870f907c4454f994c
keen <keen@openmindsresources.com>
2017-07-24 19:41:47 +0800

    Add debug extension to twig dependency

--- src/dependencies.php
+++ src/dependencies.php
@@ -5,13 +5,15 @@ $container = $app->getContainer();
 // twig renderer
 $container['view'] = function ($c) {
     $settings = $c->get('settings')['twig'];
+
     $view = new \Slim\Views\Twig($settings['template_path'], [
         'cache' => $settings['cache_path'], 
         'debug' => $settings['debug'],
         'auto_reload' => $settings['auto_reload']
     ]);
 
-    // Instantiate and add Slim specific extension
+    $view->addExtension(new Twig_Extension_Debug());
+
     $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
 
