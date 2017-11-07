ceb34334a92d2b4d41514fbe43355608baf729c6
keen <keen@openmindsresources.com>
2017-11-07 21:50:50 +0800

    code cleanup

--- src/routes.php
+++ src/routes.php
@@ -3,9 +3,5 @@
 $app->get('/test[/{name}]', \TestController::class . ':general')->setName("test-general")->add($mw);
 
 $app->get('/', function ($request, $response, $args) {
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    $data = array();
-
-    return $this->view->render($response, 'base.html', $data);
-})->setName("landing");
+    return $this->view->render($response, 'blank.html');
+});
